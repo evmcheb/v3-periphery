a026a6d16e5c473e6603d8dbd72c200cd237c0fb
evmcheb <evmcheb@users.noreply.github.com>
2022-11-15 15:20:27 +0800

    chore: pragma

--- contracts/test/PoolTicksCounterTest.sol
+++ contracts/test/PoolTicksCounterTest.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
-pragma solidity >=0.6.0;
+pragma solidity ^0.8.0;
 
 import '../libraries/PoolTicksCounter.sol';
 
